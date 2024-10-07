f2aa24b73daaac2bca010248a6f562518e4d2823
jhayjhay <jaysoncleofas.gelera@bicol-u.edu.ph>
2024-10-08 02:10:36 +0800

    vue user management system exercise

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,145 @@
 <script setup>
   import {ref} from 'vue';
+  
+  let userList = ref([]);
+  let archive = ref([]);
+  let username = ref(null);
+  let fullname = ref(null);
+  let email = ref(null);
+  let selectedUpdate = ref(false);
+  let selectedUserID = ref(null);
+  let showArchive = ref(false);
 
-  let todoList = ref([]);
-  let todo = ref(null);
+  const handleSubmit = ()=>{
+    userList.value.push({
+      id: getUserID(),
+      username: username.value,
+      fullname: fullname.value,
+      email: email.value
+    });
+    resetForm();
+  }
 
-  const addTodo = () => {
-    if (validateTodo(todo.value)) {
-      let id = todoList.value.length;
-      todoList.value.push({
-        id: ++id,
-        todo: todo.value,
-        is_done: false
-      })
-      todo.value = null
+  const getUserID = ()=>{
+    if(userList.value.length === 0){
+      return 0;
+    }else{
+      return userList.value[userList.value.length - 1].id + 1;
     }
   }
 
+  const handleUpdateOnclick = (index)=>{
+    username.value = userList.value[index].username;
+    fullname.value = userList.value[index].fullname;
+    email.value = userList.value[index].email;
+    selectedUpdate.value = true;
+    selectedUserID.value = index;
+  }
 
-  const validateTodo = (new_todo) => {
-    let index = todoList.value.findIndex((t) => t.todo === new_todo);
-    if (index !== -1) {
-      alert("Todo already exist!");
-      return false;
-    } else {
-      return true;
+  const handleSubmitUpdate = ()=>{
+    userList.value[selectedUserID.value].username = username.value;
+    userList.value[selectedUserID.value].fullname = fullname.value;
+    userList.value[selectedUserID.value].email = email.value;
+    selectedUpdate.value = false;
+    selectedUserID.value = null;
+    resetForm();
+  } 
+
+  const resetForm = ()=>{
+    username.value = null;
+    fullname.value = null;
+    email.value = null;
+  }
+
+  const handleDelete = (index)=>{
+    if (confirm(`Are you sure you want to delete ${userList.value[index].fullname} data?`)) {
+      archive.value.push(userList.value[index])
+      userList.value.splice(index, 1);
     }
+    console.log("archive",archive.value)
   }
 
-  let selectedTodo = ref(null);
-  const selectTodo = (row) => {
-    selectedTodo.value = row;
-    todo.value = row.todo;
+  const handleShowArchive = ()=>{
+    showArchive.value = true;
   }
 
-  const updateTodo = () => {
-    if (validateTodo(todo.value)) {
-      let index = todoList.value.findIndex((t) => t.id === selectedTodo.value.id);
-      index !== -1 && (todoList.value[index].todo = todo.value);
-
-
-      todo.value = selectedTodo.value = null;
-    }
+  const handleHideArchive = ()=>{
+    showArchive.value = false;
   }
 
-  const markAsDone = (row) => {
-    if (confirm(`Are you sure you want to mark ${row.todo} as done?`)) {
-      let index = todoList.value.findIndex((t) => t.id === row.id);
-      index !== -1 && (todoList.value[index].is_done = true);
-    }
+  const handleRestore = (index)=>{
+    archive.value[index].id = getUserID();
+    userList.value.push(archive.value[index]);
+    archive.value.splice(index, 1);
+    console.log("userList.value:",userList.value)
+    console.log("archive.value:",archive.value)
   }
-
-  const removeTodo = (row) => {
-    if (confirm(`Are you sure you want to delete ${row.todo}?`)) {
-      let index = todoList.value.findIndex((t) => t.id === row.id);
-      index !== -1 && todoList.value.splice(index, 1);
-    }
-  }
-
 </script>
 
 <template>
   <div class="d-flex justify-content-center container">
-      <div class="w-75">
-          <form @submit.prevent="!selectedTodo ? addTodo() : updateTodo()">
-              <div class="mb-3 mt-5">
-                <label for="todo">What's your todo?</label>
-                <div class="d-flex">
-                  <input v-model="todo" type="text" class="form-control" id="todo" placeholder="eg. Create todo application">
-                  <button v-if="!selectedTodo" class="btn btn-primary ms-3"><i class="bi bi-plus-circle-fill"></i></button>
-                  <button v-else class="btn btn-primary ms-3"><i class="bi bi-pencil-square"></i></button>
-                </div>
+    <div class="w-75 mt-5">
+      <form class="w-50 mb-5" @submit.prevent="!selectedUpdate ? handleSubmit() : handleSubmitUpdate()">
+        <input v-model="username" type="text" class="form-control mb-2" id="username" placeholder="Enter Username" required>
+        <input v-model="fullname" type="text" class="form-control mb-2" id="fullname" placeholder="Enter Fullname" required>
+        <input v-model="email" type="email" class="form-control mb-2" id="email" placeholder="Enter Email" required>
+        <div class="d-flex justify-content-between">
+          <button 
+            @click.prevent="!showArchive ? handleShowArchive() : handleHideArchive()"
+            :class="`btn btn-${!showArchive ? 'secondary' : 'success'} btn-sm`"
+          >
+            <i :class="`bi bi-${!showArchive ? 'archive' : 'people'}`"></i> Show {{ !showArchive ? "Archive" : "Active" }} Users List
+          </button>
+          <button class="btn btn-primary"><i :class="`bi bi-${!selectedUpdate ? 'person-add' : 'save2'}`"></i> {{ !selectedUpdate ? "Add User" : "Save" }}</button>
+        </div>
+      </form>
+      <div v-if="!showArchive" class="card mb-2" v-for="(user, index) in userList" :key="user.id">
+        <div class="card-body">
+          <div class="row align-items-center">
+            <div class="col-10">
+              <i class="bi bi-person-circle" id="user-icon"></i>
+              <div class="user-info d-inline-block ms-3">
+                <h4>{{ user.fullname }} ({{ user.username }}) <span class="badge text-bg-success">Active</span></h4>
+                <p>{{ user.email }}</p>
               </div>
-          </form>
-          <div @click="selectTodo(row)" v-for="row in todoList" :key="row.id" class="toast show mt-2 w-100">
-            <div class="toast-header">
-              <strong class="me-auto">{{row.todo}}</strong>
-              <button :class="`btn btn-${row.is_done ? 'success' : 'outline-secondary'} btn-sm`" @click.stop="markAsDone(row)"><i class="bi bi-check-lg"></i></button>
-              <button class="btn btn-danger btn-sm ms-1" @click.stop="removeTodo(row)"><i class="bi bi-trash-fill"></i></button>
+            </div>
+            <div class="col-2">
+              <div class="d-flex justify-content-around">
+                <button class="btn btn-primary" @click.stop="handleUpdateOnclick(index)"><i class="bi bi-pencil"></i></button>
+                <button class="btn btn-danger" @click.stop="handleDelete(index)"><i class="bi bi-trash-fill"></i></button>
+              </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div v-if="showArchive" class="card mb-2" v-for="(user, index) in archive" :key="user.id">
+        <div class="card-body">
+          <div class="row align-items-center">
+            <div class="col-10">
+              <i class="bi bi-person-circle" id="user-icon"></i>
+              <div class="user-info d-inline-block ms-3">
+                <h4>{{ user.fullname }} ({{ user.username }}) <span class="badge text-bg-danger">Deleted</span></h4>
+                <p>{{ user.email }}</p>
+              </div>
+            </div>
+            <div class="col-2">
+              <div class="d-flex justify-content-center">
+                <button class="btn btn-success" @click.stop="handleRestore(index)"><i class="bi bi-person-up"></i> Restore User</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
+  #user-icon{
+    font-size: 60px;
+  }
+
+  .user-info *{
+    margin: 0px
+  }
 </style>
